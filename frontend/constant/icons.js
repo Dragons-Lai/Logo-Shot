f5fc33806fd571956cedc5979ec4b85f3f7885f4
Dragons <dragons870409@gmail.com>
2021-10-11 18:14:20 +0800

    /constant 整合icons.js, images.js, theme.js

---
+++ frontend/constant/icons.js
@@ -0,0 +1,11 @@
+export const camera = require("../assets/icons/camera2.png");
+export const idea = require("../assets/icons/idea.png");
+export const text = require("../assets/icons/text.png");
+export const gallery = require("../assets/icons/gallery.png");
+
+export default {
+  camera,
+  idea,
+  text,
+  gallery,
+};
---
+++ frontend/constant/images.js
@@ -0,0 +1,9 @@
+export const cat = require("../assets/images/cat.jpg");
+export const dog = require("../assets/images/dog.jpg");
+export const black = require("../assets/images/black.png");
+
+export default {
+  cat,
+  dog,
+  black,
+};
