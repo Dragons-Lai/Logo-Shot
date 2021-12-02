22bd4cb58ebf1ad82e768332fd07c18f660a465f
Dragons <dragons870409@gmail.com>
2021-12-02 09:59:13 +0800

    後端產生html report

--- backend/app.py
+++ backend/app.py
@@ -155,8 +155,7 @@ def sql_command2(cur, colnames, tablename, caseno_list):
     # print("conditions: ", conditions, file=sys.stdout)
     # print("SQL: ", "SELECT {} FROM {} WHERE {}".format(selected, tablename, conditions), file=sys.stdout)
     cur.execute("SELECT {} FROM {} WHERE {}".format(selected, tablename, conditions))
-    df = pd.DataFrame(cur.fetchall()).astype(str)
-    df.columns = colnames
+    df = pd.DataFrame(cur.fetchall(), columns=colnames).astype(str)
     df = df.groupby(df['caseno'], as_index=False).aggregate(lambda x: "/".join(x))
     return df
 
@@ -250,7 +249,27 @@ import slimon
 model = slimon.Model()
 from Search import search
 
-def load_images(caseno_list):
+def create_html(TYPE, SEARCH, TIME, resultPath_list):
+    dirr = time.strftime("/home/metadragons/backend/SearchHtmlResults/{}/%Y-%m-%d").format(TYPE)
+    # print("dirr: ", dirr)
+    os.makedirs(dirr, exist_ok=True)
+    html_str = '<div class="t">\n'
+    html_str += '\t<h2>{} Search:</h2>\n'.format(TYPE)
+    if TYPE == "Image":
+        html_str += '\t<img src="../../../../../..{}" width="300"/>\n'.format(SEARCH)
+    elif TYPE == "Text":
+        html_str += '\t<h3>QUERY={}</h3>\n'.format(SEARCH)
+    html_str += '\t<h2>Results:</h2>\n'
+    for i in range(len(resultPath_list)):
+        html_str += '\t<h3>{}.</h3>\n'.format(i+1)
+        html_str += '\t<img src="../../../../../..{}" width="300"/>\n'.format(resultPath_list[i])
+    html_str += '</div>'
+    Html_file= open("{}/{}.html".format(dirr, TIME),"w")
+    Html_file.write(html_str)
+    Html_file.close()
+    return html_str
+
+def load_images(TYPE, SEARCH, TIME, caseno_list):
     result_list = []
     # caseno_list = [99018887, 100048442, 100006294, 100036013, 100015475, 100045485, 100053242, 100053158, 98017486, 100048244, 100034136, 100015575, 99059834, 100039852, 100020857, 100039089, 100015683, 100030430, 99059734, 100050261, 100041025, 100034969, 100058002, 100035279, 100047596, 100048242, 100047022, 100033212, 100047019, 100048739, 99056287, 99042253, 99022071, 100018260, 100043720, 100035291, 99061105, 99055598, 100055899, 100022190, 100037752, 100032097, 98030087, 100041404, 100034168, 100012916, 99040055, 100047249, 100037818, 100046101, 100000214, 100039351, 100043045, 100046931, 100040241, 100010120, 99057907, 100036572, 100035000, 100027168, 100037388, 99042111, 100028773, 100032160, 100018054, 100001572, 99038458, 100040028, 100008473, 99054248, 100040320, 100034946, 100029091, 100038959, 99035163, 100027546, 99041348, 100021585, 99059678, 97049456, 100053398, 100022946, 100040031, 100028393, 100039333, 100010723, 100034580, 100046150, 100041676, 100030440, 99057980, 100036995, 100045130, 100025812, 100036394, 100015200, 100044424, 100021701, 100040164, 100040873]
     # for caseno in caseno_list:
@@ -265,13 +284,17 @@ def load_images(caseno_list):
             # print('r["metadata"]: ', r["metadata"], file=sys.stdout)            
             pil_img = Image.open(r["Path"], mode='r') # reads the PIL image
             # print(os.path.getsize(r["Path"]))
+            (width, height) = (pil_img.width // 2, pil_img.height // 2)
+            # print((width, height))
+            pil_img = pil_img.resize((width, height))
             byte_arr = io.BytesIO()
-            pil_img.save(byte_arr, format='PNG', quality = 10) # convert the PIL image to byte array
+            pil_img.save(byte_arr, format='PNG', quality=50) # convert the PIL image to byte array
             encoded_img = encodebytes(byte_arr.getvalue()).decode('ascii') # encode as base64        
             base64Image_list.append(encoded_img)
             metadata_list.append(r["metadata"])
         except:
             pass
+    html_str = create_html(TYPE, SEARCH, TIME, [r["Path"] for r in result_list])
     return base64Image_list, metadata_list
 
 @app.route('/function5', methods=['POST'])
@@ -284,8 +307,11 @@ def function5():
     caseno_list1 = []
     if photo != "null":
         # print(request.form['name'], file=sys.stdout)   
-        filePath = "/home/metadragons/backend/uploadedImages/{}.png".format(request.form['name'])
-        print("filePath:", filePath, file=sys.stdout) 
+        dirr = time.strftime("/home/metadragons/backend/uploadedImages/%Y-%m-%d")
+        # print("dirr: ", dirr)
+        os.makedirs(dirr, exist_ok=True)        
+        filePath = "{}/{}.png".format(dirr, request.form['name'])
+        print("ImageSearch:", filePath, file=sys.stdout) 
         with open(filePath, "wb") as f:
             img = base64.decodebytes(photo.encode('ascii'))
             f.write(img)
@@ -296,7 +322,7 @@ def function5():
         print("Time Spent(Image_Model): {}s".format(EndTime2 - startTime2), file=sys.stdout)
     caseno_list2 = []    
     if searchQuery != "":
-        print("searchQuery:", searchQuery, file=sys.stdout) 
+        print("TextSearch:", searchQuery, file=sys.stdout) 
         check1 = True if request.form["check1"] == "true" else False
         check2 = True if request.form["check2"] == "true" else False
         check3 = True if request.form["check3"] == "true" else False
@@ -307,12 +333,15 @@ def function5():
         # print("check4:", check4, file=sys.stdout) 
         startTime3 = time.time()
         # caseno_list2 = random.sample(caseno_1000, 20)
-        caseno_list2 = search(searchQuery,20,[check1,check2,check3])
+        caseno_list2 = search(searchQuery,50,[check1,check2,check3])
         EndTime3 = time.time()
         print("Time Spent(Text_Model): {}s".format(EndTime3 - startTime3), file=sys.stdout)        
-    startTime4 = time.time()    
-    base64Image_list1, metadata_list1 = load_images(caseno_list1)  
-    base64Image_list2, metadata_list2 = load_images(caseno_list2) 
+    startTime4 = time.time()
+    base64Image_list1, metadata_list1, base64Image_list2, metadata_list2 = [], [], [], []
+    if photo != "null":   
+        base64Image_list1, metadata_list1 = load_images("Image", filePath, request.form['name'], caseno_list1) 
+    if searchQuery != "":
+        base64Image_list2, metadata_list2 = load_images("Text", searchQuery, request.form['name'], caseno_list2) 
     EndTime4 = time.time()
     print("Time Spent(load_images): {}s".format(EndTime4 - startTime4), file=sys.stdout)
     EndTime = time.time()
